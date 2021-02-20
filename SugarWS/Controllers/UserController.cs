b686fb4f1b8ad24fdc0a8075dbb691bc7737527c
Arnaud <arnaudbruno@gmail.com>
2021-02-20 22:31:42 +0100

    Ajout userController

---
+++ SugarWS/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SugarWS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Core.User Get(int id)
+        {
+            if(id ==1)
+                return new Core.User("Eden", "Martin",DateTime.Now);
+            else
+                return new Core.User("Jean", "Valjean",DateTime.Now);
+        }
+
+        [HttpPost]
+        public Core.User Create(string firstName, string lastName)
+        {
+            return new Core.User(firstName,lastName ,DateTime.Now);
+        }
+    }
+}
