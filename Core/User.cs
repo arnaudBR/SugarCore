c1a6706acd07c378f293b5de2db0bf40afbfa297
Arnaud <arnaudbruno@gmail.com>
2021-02-20 21:37:26 +0100

    Add Core project

---
+++ Core/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Core
+{
+    public class User
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName; 
+        private DateTime _birthDate; 
+
+
+        public string FirstName
+        {
+            get { return _firstName ; }
+        }  
+
+        public string LastName
+        {
+            get { return _lastName ; }
+        }  
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate ; }
+        }
+
+
+        public User(string firstName, string lastName, DateTime birthDate)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._birthDate = birthDate;
+        }
+    }
+}
